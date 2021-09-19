c978cc3b4a5c0089b76caed8914cb1197dc29cbb
Mr-Destructive <gormeet711@gmail.com>
2021-09-19 10:42:14 +0530

    Debug set to false

--- WorldAtlasAPI/settings.py
+++ WorldAtlasAPI/settings.py
@@ -30,16 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['herokuapp.com', '127.0.0.1','localhost:8000']
-
 
 # Application definition
 
