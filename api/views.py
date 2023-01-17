4ce7f991a0bcddc6019e201297740deb0c79bb37
mr_destructive <gormeet711@gmail.com>
2023-01-17 21:52:03 +0530

    fix: api revamp with drf

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from .models import Places
 from rest_framework.views import APIView
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from django.http import JsonResponse
@@ -44,7 +45,6 @@ class GetList(APIView):
         places = Places.objects.filter(name__startswith=letter).values_list(
             "name", flat=True
         )
-        print(list(places))
         return JsonResponse({letter: list(places)})
 
 
@@ -52,3 +52,28 @@ class PlaceViewSet(ModelViewSet):
     permission_classes = (IsAdminUser,)
     queryset = Places.objects.all()
     serializer_class = PlacesSerializer
+
+    @action(detail=False, methods=['GET',], url_path='lists/(?P<letter>[^/.]+)')
+    def lists(self, request, letter:str):
+        places = Places.objects.filter(name__startswith=letter).values_list(
+            "name", flat=True
+        )
+        return JsonResponse({letter: list(places)})
+
+    @action(detail=False, methods=['GET',], url_path='get/(?P<letter>[^/.]+)')
+    def get_random_place(self, request, letter):
+        places = Places.objects.filter(name__startswith=letter).values_list('name', flat=True)
+        if places:
+            place = random.choice(places)
+        else:
+            return JsonResponse({'message': 'No place found'}, status=status.HTTP_404_NOT_FOUND)
+        return JsonResponse({letter: place})
+
+    @action(detail=False, methods=['GET',], url_path='exists/(?P<place>[^/.]+)')
+    def place_exists(self, request, place):
+        place_exists = Places.objects.filter(name=place).exists()
+        if place_exists:
+            return JsonResponse({"result": True})
+        else:
+            return JsonResponse({"result": False})
+
